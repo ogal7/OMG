fa2cf00d763d935907edfef444564cbc2ce2f4b0
michaelwoo <michaelwoo99@yahoo.com>
2017-03-31 12:31:11 -0400

    fix maps size

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 
+
+
+// fx(airport, year) get total flights
+// fx(airport, year) get delays
+// fx(airport, year) get flights info return ["cancelled","on time", total, delayed diverted]
+// fx(airport, year) get delays info
+  //return [late aircraft, weather, security, national aviation system
+
+//d3 stuff dynamic
+//link circles with their airport data
+//create bar graph with d3
+
+
+
