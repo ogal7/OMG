6f5f6817caae508a74b9988c54695b25f996b01b
mwoo99 <michaelwoo99@yahoo.com>
2017-04-02 14:56:05 -0400

    graphs html and js

--- app.js
+++ app.js
@@ -11,5 +11,41 @@
 //link circles with their airport data
 //create bar graph with d3
 
+var totalscontainer = document.getElementById("totals");
+var delayscontainer = document.getElementById("delays");
+var reqId;
 
+/*
+var change = function(e) {
+	console.log("haha");
+	if (this.getAttribute("fill") == "pink") {
+		container.removeChild(this);
+		var c = createCircle(Math.random()*500,Math.random()*500);	
+		container.appendChild(c);
+		e.stopPropagation();
+	}
+	else {
+		this.setAttribute("fill", "pink");
+		e.stopPropagation();
+	}	
+}
+*/
 
+var createCircle = function (x,y,r) {
+	var c =	document.createElementNS("http://www.w3.org/2000/svg", "circle");
+    c.setAttribute("fill", "blue");
+    c.setAttribute("cx", x);
+    c.setAttribute("cy", y);
+    c.setAttribute("r", r);    
+    c.addEventListener("click", change);   //change to redirect to graphs
+    return c;
+}
+
+/* 
+var clear = function() {
+	var list = document.getElementsByTagName("circle");
+	while (list.length != 0) {
+		container.removeChild(list[0]);
+	}
+}
+*/
