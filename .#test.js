828b3812b7ca01daf267153e227d556943f92f37
gTopa <gtopa@stuy.edu>
2017-04-04 07:51:17 -0400

    work

---
+++ .#test.js
@@ -0,0 +1,1 @@
+Jeff@Jeff.local.5995
--- static/test.js
+++ static/test.js
@@ -1,10 +1,16 @@
+var dataSet=[];
+
 function loadDoc() {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-	console.log(this.responseText);
+	return JSON.parse(this.responseText);
     };
   };
   xhttp.open("GET", "http://127.0.0.1:5000/list/", true);
   xhttp.send();
 };
+
+
+dataSet=JSON.parse(loadDoc());
+console.log(loadDoc());
